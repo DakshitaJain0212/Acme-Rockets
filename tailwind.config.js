d8eebea19e54db414ad7aa170b19fcaebc965e9a
DakshitaJain0212 <dakshitaj02@gmail.com>
2023-09-21 15:32:23 +0530

    Update Files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.html'],
+  content: ["./build/**/*.{html,js}"],
   theme: {
     extend: {
       screens:{
         'widescreen': {'raw': '(min-aspect-ratio: 3/2)'},
-        'tallscreen': {'raw': '(min-aspect-ratio: 1/2)'}
-      }
+        'tallscreen': {'raw': '(max-aspect-ratio: 13/20 )'}
+      },
+      keyframes: {
+        'open-menu' : {
+          '0%': { tranform: 'scaleY(0)'},
+          '80%': { tranform: 'scaleY(1.2)'},
+          '100%': { tranform: 'scaleY(1)'},
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+
+      },
     },
   },
   plugins: [],
